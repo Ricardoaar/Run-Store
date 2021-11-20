7e8dd9e42db74fb2bbac8b20a9e8cac19d21d3bf
shemaxd <chemagr25@gmail.com>
2021-11-19 20:38:40 -0500

    bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,8 @@
-<h1>Hola C:</h1>
+@extends('layouts.front.layout')
+
+@section('content')
+<div class="container-fluid d-flex justify-content-center align-items-center">
+<img src="https://i.blogs.es/2200eb/marvel-logo-sony-derechos-25-millones/450_1000.jpeg" alt="">
+</div>
+
+@endsection
